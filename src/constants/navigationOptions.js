11d15239dcae6b8c132ad66e9338a2a9072ad644
Hasan Iqtedar <hasan.iqtedar@gmail.com>
2022-09-02 19:37:57 +0500

    Add navigation options

---
+++ src/constants/navigationOptions.js
@@ -0,0 +1,42 @@
+export const navigationOptions = [
+  {
+    name: "Dashboard",
+    path: "/",
+  },
+  {
+    name: "Pending Bookings",
+    path: "/pending-bookings",
+  },
+  {
+    name: "Upcomming Bookings",
+    path: "/upcomming-bookings",
+  },
+  {
+    name: "Locations",
+    path: "/locations",
+  },
+  {
+    name: "Categories",
+    path: "/categories",
+  },
+  {
+    name: "Stylists",
+    path: "/stylists",
+  },
+  {
+    name: "Slots",
+    path: "/slots",
+  },
+  {
+    name: "History",
+    path: "/history",
+  },
+  {
+    name: "Users",
+    path: "/users",
+  },
+  {
+    name: "Settings",
+    path: "/settings",
+  },
+];
