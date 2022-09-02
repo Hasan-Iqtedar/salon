b91cf06e9c6b77733e1ae0cefebe7d0a30915c4a
Hasan Iqtedar <hasan.iqtedar@gmail.com>
2022-09-02 16:43:42 +0500

    Set up router

--- src/index.js
+++ src/index.js
@@ -1,12 +1,27 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import "./styles/index.css";
 import App from "./App";
+import Login from "./pages/Login";
+import RequireAuth from "./components/RequireAuth";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { AuthProvider } from "./contexts/auth";
+import "./styles/index.css";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <AuthProvider>
-    <App />
+    <BrowserRouter>
+      <Routes>
+        <Route
+          path="/*"
+          element={
+            <RequireAuth>
+              <App />
+            </RequireAuth>
+          }
+        />
+        <Route path="/login" element={<Login />} />
+      </Routes>
+    </BrowserRouter>
   </AuthProvider>
 );
