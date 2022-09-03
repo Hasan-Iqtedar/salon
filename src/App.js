950c9c43004a8f4a0d2c78e1dbf58f4984216ae7
Hasan Iqtedar <hasan.iqtedar@gmail.com>
2022-09-03 16:08:16 +0500

    Restructure routes

--- src/App.js
+++ src/App.js
@@ -1,10 +1,32 @@
 import Dashboard from "./pages/Dashboard";
+import { Route, Routes } from "react-router-dom";
+import PendingBookings from "./pages/PendingBookings";
+import UpcomingBookings from "./pages/UpcomingBookings";
+import NavigationPanel from "./components/NavigationPanel";
+import Container from "./components/Container";
+import Header from "./components/Header";
 import "./styles/app.css";
 
 function App() {
   return (
-    <div className="App">
-      <Dashboard />
+    <div className="app">
+      <NavigationPanel />
+      <Container>
+        <Header />
+        <Routes>
+          <Route path="/dashboard" element={<Dashboard />} />
+          <Route path="/pending-bookings" element={<PendingBookings />} />
+          <Route path="/upcoming-bookings" element={<UpcomingBookings />} />
+          <Route />
+        </Routes>
+      </Container>
+      {/* container 
+              -Header
+              -Table
+                  --Table Header
+                  --Table Content
+              -Footer
+        */}
     </div>
   );
 }
