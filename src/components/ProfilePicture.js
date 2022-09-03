d7870dede1595eef44dc1d27aba9d73506a3c216
Hasan Iqtedar <hasan.iqtedar@gmail.com>
2022-09-03 15:02:05 +0500

    Add component for profile picture

---
+++ src/components/ProfilePicture.js
@@ -0,0 +1,7 @@
+import "../styles/profile-picture.css";
+
+const ProfilePicture = (props) => {
+  return <div className="profile-picture" style={props.style}></div>;
+};
+
+export default ProfilePicture;
