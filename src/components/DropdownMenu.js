2a04f37aba32fb89d5ff14909c47e808f6ce1263
Hasan Iqtedar <hasan.iqtedar@gmail.com>
2022-09-05 19:51:45 +0500

    Clean code

--- src/components/DropdownMenu.js
+++ src/components/DropdownMenu.js
@@ -1,26 +1,14 @@
 import { AiFillCaretDown } from "react-icons/ai";
-import { FaTrash } from "react-icons/fa";
-import { IoPencil } from "react-icons/io5";
-
 import "../styles/dropdownMenu.css";
 
 const DropdownMenu = (props) => {
-
   return (
     <div className="dropdown-menu">
       <div className="title">
         {props.title}
         <AiFillCaretDown className="icon" />
       </div>
-      <div className="dropdown-menu-content">
-        {/* <div>
-          <IoPencil className="icon" /> Details
-        </div>
-        <div>
-          <FaTrash className="icon" /> Delete
-        </div> */}
-        {props.children}
-      </div>
+      <div className="dropdown-menu-content">{props.children}</div>
     </div>
   );
 };
