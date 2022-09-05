8dc79f9fc8519f59f0f04534aa49f550cc1f81c7
Hasan Iqtedar <hasan.iqtedar@gmail.com>
2022-09-05 15:59:24 +0500

    Add ids

--- src/constants/data.js
+++ src/constants/data.js
@@ -1,17 +1,20 @@
 export const pendingBookings = [
   {
+    id: 0,
     name: "Dale Philips",
     service: "Braided",
     time: "10:00 - 11:00 AM",
     stylist: "Alia David",
   },
   {
+    id: 1,
     name: "Dale Philips",
     service: "Braided",
     time: "10:00 - 11:00 AM",
     stylist: "Alia David",
   },
   {
+    id: 2,
     name: "Dale Philips",
     service: "Braided",
     time: "10:00 - 11:00 AM",
@@ -21,18 +24,21 @@ export const pendingBookings = [
 
 export const upcomingBookings = [
   {
+    id: 0,
     name: "Dale Philips",
     service: "Braided",
     time: "10:00 - 11:00 AM",
     stylist: "Alia David",
   },
   {
+    id: 1,
     name: "Dale Philips",
     service: "Braided",
     time: "10:00 - 11:00 AM",
     stylist: "Alia David",
   },
   {
+    id: 2,
     name: "Dale Philips",
     service: "Braided",
     time: "10:00 - 11:00 AM",
