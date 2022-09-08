d31b3c23ddb9e1fffb7eed171357cb6078b6ebe9
Hasan Iqtedar <hasan.iqtedar@gmail.com>
2022-09-08 21:06:20 +0500

    Add filter icon

--- src/components/TableHeader.js
+++ src/components/TableHeader.js
@@ -1,5 +1,6 @@
 import { HiLocationMarker } from "react-icons/hi";
 import { AiFillCaretDown } from "react-icons/ai";
+import filterImage from "../assets/filterImage.png";
 
 import "../styles/tableHeader.css";
 
@@ -7,11 +8,15 @@ const TableHeader = (props) => {
   return (
     <div className="table-header">
       <h2>{props.title}</h2>
-      <div className="container">
-        {props.default ? <button>Default</button> : ""}
-        <div className="location">
-          <HiLocationMarker className="icon" /> Location: All
-          <AiFillCaretDown className="icon small" />
+
+      <div className="container" >
+        <div className="center-container" style={{width: '100%'}} >
+          {props.default ? <button>Default</button> : ""}
+          <div className="location">
+            <HiLocationMarker className="icon" /> Location: All
+            <AiFillCaretDown className="icon small" />
+          </div>
+          <img src={filterImage} alt=""></img>
         </div>
       </div>
     </div>
