c9c86b2d307560d1432354c20aad597c00c8e124
Hasan Iqtedar <hasan.iqtedar@gmail.com>
2022-09-08 21:53:32 +0500

    Update styles

--- src/components/NavigationPanel.js
+++ src/components/NavigationPanel.js
@@ -3,6 +3,7 @@ import { RiLogoutBoxRFill } from "react-icons/ri";
 import { useAuth } from "../contexts/auth";
 import Logo from "./Logo";
 import { navigationOptions } from "../constants/navigationOptions";
+import signOutImage from "../assets/signOutImage.png";
 import "../styles/navigationPanel.css";
 
 const NavigationPanel = (props) => {
@@ -36,7 +37,12 @@ const NavigationPanel = (props) => {
         </ul>
       </div>
       <div className="sign-out-container">
-        <RiLogoutBoxRFill className="icon" />
+        {/* <RiLogoutBoxRFill className="icon" /> */}
+        <img
+          src={signOutImage}
+          alt=""
+          style={{ width: "19px", height: "24px" }}
+        ></img>
         <button className="sign-out" onClick={signOut}>
           Sign out
         </button>
